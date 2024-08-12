ca32a251e01794bb1d995c1c9b2a82d29c3100ce
sindydwns <sindydwns@naver.com>
2024-08-12 21:52:23 +0900

    fix

---
+++ openvoice/install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+git clone https://github.com/myshell-ai/OpenVoice
+mv -r OpenVoice/* openvoice-repo/
+cd openvoice-repo
+wget https://myshell-public-repo-host.s3.amazonaws.com/openvoice/checkpoints_v2_0417.zip
+unzip checkpoints_v2_0417.zip
+rm checkpoints_v2_0417.zip
+
+echo "alias ju='jupyter notebook . --allow-root --ip 0.0.0.0 --no-browser'" >> ~/.bashrc
