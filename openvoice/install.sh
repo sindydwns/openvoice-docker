a4314ff79cf68d026ed47008888016dabbc87756
sindydwns <sindydwns@naver.com>
2024-08-13 08:34:09 +0900

    fix

--- openvoice/install.sh
+++ openvoice/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 git clone https://github.com/myshell-ai/OpenVoice
 mv -r OpenVoice/* openvoice-repo/
+pip install -r requirements.txt
+pip install -e .
 cd openvoice-repo
 wget https://myshell-public-repo-host.s3.amazonaws.com/openvoice/checkpoints_v2_0417.zip
 unzip checkpoints_v2_0417.zip
