15ad3c9392f0a97fdc90f852f161b287c917197d
sindydwns <sindydwns@naver.com>
2024-08-17 20:54:40 +0900

    update prompt

--- openvoice-repo/chatbot.py
+++ openvoice-repo/chatbot.py
@@ -8,33 +8,22 @@ memory = ConversationBufferMemory()
 class RecommendChatbot():
     def __init__(self, content):
         load_dotenv()
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=100)
-        self.text_chunks = text_splitter.split_text(content)
+        print(content)
+        chat_model = ChatOpenAI(temperature=0, model_name = 'gpt-4o-mini')
+        chat_prompt = ChatPromptTemplate.from_messages([
+            SystemMessagePromptTemplate.from_template(f"다음 CONTENT 내용과 관련된 질문에만 대답해.\n\nCONTENT:\n{content}"),
+            HumanMessagePromptTemplate.from_template("{history}"),
+            HumanMessagePromptTemplate.from_template("{input}")
+        ])
 
-        # 템플릿 정의
-        prompt_template = PromptTemplate(
-            input_variables=["input"],
-            template="제품 정보: {input}\n\n질문에 대한 간단한 요약 답변(3줄 이내):"
-        )
-
-        # LLMChain 생성
-        self.chat_chain = LLMChain(
-            llm=ChatOpenAI(model_name="gpt-4o-mini"),
-            prompt=prompt_template,
+        self.chat_conversation_chain = ConversationChain(
+            llm=chat_model,
+            prompt=chat_prompt,
             memory=memory
         )
 
-    def pred(self, user_input):
-        # 각 텍스트 조각에 대해 질문을 처리하고 답변을 생성
-        response_summary = ""
-        for chunk in self.text_chunks:
-            combined_input = f"{chunk}\n질문: {user_input}"
-            response = self.chat_chain.predict(input=combined_input)
-            response_summary = response.strip()  # 마지막 조각의 답변을 사용하여 요약
-
-            if response_summary:  # 첫 번째로 의미 있는 요약만 사용
-                return response_summary
-        return ""
+    def pred(self, chat):
+        return self.chat_conversation_chain.predict(input=chat)
 
 from langchain.chains.summarize import load_summarize_chain
 from langchain.chat_models import ChatOpenAI
@@ -48,7 +37,7 @@ class SummarizeChatbot():
         load_dotenv()
         chat_model = ChatOpenAI(temperature=0, model_name = 'gpt-4o-mini')
         chat_prompt = ChatPromptTemplate.from_messages([
-            SystemMessagePromptTemplate.from_template(f"{content}의 내용과 관련된 이야기만 반응해."),
+            SystemMessagePromptTemplate.from_template(f"다음 CONTENT 내용과 관련된 질문에만 대답해.\n\nCONTENT:\n{content}"),
             HumanMessagePromptTemplate.from_template("{history}"),
             HumanMessagePromptTemplate.from_template("{input}")
         ])
@@ -58,8 +47,7 @@ class SummarizeChatbot():
             prompt=chat_prompt,
             memory=memory
         )
-        
-        
+
         self.text_splitter = RecursiveCharacterTextSplitter(separators=["\n\n", "\n"], chunk_size=3000, chunk_overlap=300)
         self.docs = self.text_splitter.create_documents([content])
         
@@ -71,12 +59,12 @@ class SummarizeChatbot():
             {text}를 시청하지 못하면 알 수 없을 내용들을 작성해
         '''
         
-        MAP_PROMPT = PromptTemplate(template=map_prompt_template, input_variables=["text"])
-        COMBINE_PROMPT = PromptTemplate(template=combine_prompt_template, input_variables=["text"])
+        map_prompt = PromptTemplate(template=map_prompt_template, input_variables=["text"])
+        combine_prompt = PromptTemplate(template=combine_prompt_template, input_variables=["text"])
         
         chat_model = ChatOpenAI(temperature=0, model_name = 'gpt-4o-mini')
         self.chain = load_summarize_chain(chat_model, chain_type="map_reduce", return_intermediate_steps=True,
-                                    map_prompt=MAP_PROMPT, combine_prompt=COMBINE_PROMPT)
+                                    map_prompt=map_prompt, combine_prompt=combine_prompt)
     def pred(self, chat):
         if chat == "":
             print("empty case: ")
